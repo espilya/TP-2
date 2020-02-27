7f64a3c1f639934e48dca4575a43eb5257c21844
prostomisha <43436992+prostomisha@users.noreply.github.com>
2020-02-27 19:13:45 +0100

    Update RoadMap.java

--- v1/src/simulator/model/RoadMap.java
+++ v1/src/simulator/model/RoadMap.java
@@ -1,5 +1,6 @@
 package simulator.model;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -9,48 +10,84 @@ public class RoadMap {
 	private List<Vehicle> vehicleList;
 	private Map<String,Junction>  junctionMap;
 	private Map<String,Vehicle> vehicleMap;
-
-	protected RoadMap(){} // not sure that it was needed in presentation
-	//ClassRoad Map has a single package protected constructor without arguments, it simply
-	// initializes the data-structures mentioned above.
+	private Map<String, Road> roadMap;
+	protected RoadMap(){} 
+	
 
 	void addJunction(Junction j) {
+		junctionList.add(j);
+		// TO DO: Check if the element exists in the junctionMap
+		junctionMap.put(j._id, j);
 		
 	}
 	
 	void addRoad(Road r) {
+		roadList.add(r);
+		// TO DO: Check if the element exists in the roadMap
+		
+		//???¿¿¿(ii) the junctions that connect the road exists in the road-map.???¿¿¿
+		roadMap.put(r._id, r);
 		
 	}
 	
 	void addVehicle(Vehicle v) {
+		vehicleList.add(v);
+		// TO DO: Check if the element exists in the vehicleMap
 		
+		//???¿¿¿(ii) the itinerary is valid, i.e., there are roads that connect each consecutive
+		//junctions of the itinerary.???¿¿¿
+		vehicleMap.put(v._id, v);
 	}
 	
 	public Junction getJunction(String id) {
-		
+		Junction key;
+		key = junctionMap.get(id);
+		return key;
+		// returns the object with identifier id, and null
+		//if no such object exists.
+
 	}
 	
 	public Road getRoad(String id) {
-		
+		Road key;
+		key = roadMap.get(id);
+		return key;
+		// returns the object with identifier id, and null
+		//if no such object exists.
 	}
 	
 	public Vehicle getVehicle(String id) {
+		Vehicle key;
+		key = vehicleMap.get(id);
+		return key;
+		// returns the object with identifier id, and null
+		//if no such object exists.
 		
 	}
 	
 	public List<Junction>getJunctions(){
-		
+		List<Junction>getJuncs = Collections.unmodifiableList(junctionList);
+		return getJuncs;
+		// may be incorrect but looks good
 	}
 	
-	public List<Roads>getRoads(){
-		
+	public List<Road>getRoads(){
+		List<Road>getRoads = Collections.unmodifiableList(roadList);
+		return getRoads;
 	}
 	
 	public List<Vehicle>getVehicles(){
-		
+		List<Vehicle>getVehicles = Collections.unmodifiableList(vehicleList);
+		return getVehicles;
 	}
 	
 	void reset() {
+		junctionList.clear();
+		roadList.clear();
+		vehicleList.clear();
+		junctionMap.clear();
+		roadMap.clear();
+		vehicleMap.clear();
 		
 	}
 	
